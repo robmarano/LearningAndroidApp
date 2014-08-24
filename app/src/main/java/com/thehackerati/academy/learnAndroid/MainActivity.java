14b47e49c776393ca25542efc306c925a54df6a0
Rob Marano <rob@thehackerati.com>
2014-08-24 17:45:56 -0400

    As per http://developer.android.com/training/basics/firstapp/index.html, implemented a simple Android app to display what the user types.

--- app/src/main/java/com/thehackerati/academy/learnAndroid/MainActivity.java
+++ app/src/main/java/com/thehackerati/academy/learnAndroid/MainActivity.java
@@ -1,12 +1,17 @@
 package com.thehackerati.academy.learnAndroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "com.thehackerati.academy.learnAndroid.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +37,15 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
